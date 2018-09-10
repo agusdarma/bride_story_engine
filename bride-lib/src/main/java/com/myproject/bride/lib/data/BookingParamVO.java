fa8f9f4b4db3e4bf3e526e2da14067f5ea8b223f
agusdarma <agus.darma@emobile.com.sg>
2018-09-10 16:39:18 +0700

    update

--- bride-lib/src/main/java/com/myproject/bride/lib/data/BookingParamVO.java
+++ bride-lib/src/main/java/com/myproject/bride/lib/data/BookingParamVO.java
@@ -5,19 +5,21 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 public class BookingParamVO implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private String emailUserLogin;
+	private String email;
 
 	@Override
 	public String toString() {
 		return ReflectionToStringBuilder.toString(this);
 	}
 
-	public String getEmailUserLogin() {
-		return emailUserLogin;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setEmailUserLogin(String emailUserLogin) {
-		this.emailUserLogin = emailUserLogin;
+	public void setEmail(String email) {
+		this.email = email;
 	}
+
+	
 
 }
